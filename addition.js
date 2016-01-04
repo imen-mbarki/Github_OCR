54a3f9bdbb818ad333c77c21d2c6527299bd56df
imen mbarki <imenseghaier@MacBook-Air-de-imen.local>
2016-01-04 12:17:25 +0100

    Ajouter la fonction addition

--- addition.js
+++ addition.js
@@ -1,2 +1,7 @@
 var numero = Number(prompt("Entrez votre numéro:"));
 var numeroDeux = Number(prompt("Entrez un deuxième numéro:"));
+function addition(){
+	var resultat = numero+ numeroDeux;
+	return resultat;
+}
+	alert("La somme est égale à "+ addition());
